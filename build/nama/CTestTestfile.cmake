fd47db29ff3b5aa30a2e08812904f5fabcd8c93f
Gilbran Mahda <gbrnmhd89@gmail.com>
2025-03-09 05:07:24 +0700

    Delete build directory

--- build/nama/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gilbran/MagangIRIS/MagangIRIS-ROS/src/nama
-# Build directory: /home/gilbran/MagangIRIS/MagangIRIS-ROS/build/nama
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/tugas/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gilbran/MagangIRIS/MagangIRIS-ROS/src/tugas
-# Build directory: /home/gilbran/MagangIRIS/MagangIRIS-ROS/build/tugas
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
